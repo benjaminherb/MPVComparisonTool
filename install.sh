f34c29fcf92f9b1ed6c0a40fdd7f1a29621913e9
Benjamin Herb <github@benjaminherb.de>
2025-05-22 13:28:58 +0200

    Added chmod to install script

--- install.sh
+++ install.sh
@@ -1,8 +1,23 @@
 SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "${SCRIPT_PATH}" || exit
 
-mkdir -p ~/.local/share/mpvcomp/
-cp ./mpvcomp ~/.local/share/mpvcomp/
-cp ./video_comparison_tool.lua ~/.local/share/mpvcomp/
-ln -s ~/.local/share/mpvcomp/mpvcomp ~/.local/bin/mpvcomp
+SHARE_DIR="$HOME/.local/share/mpvcomp"
+BIN_DIR="$HOME/.local/bin"
+
+echo "Installing MPVComparisonTool"
+
+echo "# Making script executable"
+chmod +x ./mpvcomp
+
+echo "# Creating directory ${SHARE_DIR}"
+mkdir -p "${SHARE_DIR}"
+
+echo "# Copying files"
+cp ./mpvcomp "${SHARE_DIR}"
+cp ./video_comparison_tool.lua "${SHARE_DIR}"
+
+echo "# Creating symlink ${BIN_DIR}/mpvcomp"
+ln -sf "${SHARE_DIR}/mpvcomp" "${BIN_DIR}/mpvcomp"
+
+echo "USAGE: mpvcomp 1.mp4 2.mp4 [...]"
 
